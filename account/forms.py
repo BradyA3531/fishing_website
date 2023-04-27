a00168deb4beac3debe4181245ebe82bac6fd321
Brady Anderson <brady3531@gmail.com>
2023-04-27 14:49:32 -0500

    started login system and styled forms
    Co-authored-by: Mitzel, Zach <zachary.mitzel@ndsu.edu>
    Co-authored-by: ScCollins21 <ScCollins21@users.noreply.github.com>

--- account/forms.py
+++ account/forms.py
@@ -1,8 +1,15 @@
 from django.forms import ModelForm
 from .models import Account
+from django import forms
 
 class accountForm(ModelForm):
     class Meta:
         model = Account
         fields = ['email', 'password', 'fname', 'lname', 'addressline1', 'city', 'state', 'zipcode', 'sec_answer']
         
+        
+
+class loginForm(ModelForm):
+    class Meta:
+        model = Account
+        fields = ['email', 'password']
--- account/views.py
+++ account/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import accountForm
+from .forms import loginForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from .models import Account
 
 # Create your views here.
 
@@ -9,6 +13,17 @@ def account_view(request):
 
 #Reminder: Add model stuff later for username/email and password login
 def login_view(request):
+
+    if request.method == 'POST':
+        inputEmail = request.POST.get('email')
+        inputPassword = request.POST.get('password')
+        account = Account.objects.get(email = inputEmail)
+        if(inputPassword ==  account.password):
+            print(inputPassword)
+            print(account.password)
+            return redirect('storefront')
+    
+
     return render(request,'account/login.html')
 
 def register_view(request):
@@ -18,6 +33,7 @@ def register_view(request):
         form = accountForm(request.POST)
         if form.is_valid():
             form.save()
+            redirect('login')
 
     context = {'form' : form}
     return render(request, 'account/register.html', context)
